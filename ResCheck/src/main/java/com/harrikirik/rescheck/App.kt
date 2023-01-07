60c6c1307219292dc71398407bdbd3eb512dc196
Harri Kirik <harri.kirik@lab.mobi>
2023-01-07 22:28:00 +0200

    Update versioning logic

--- ResCheck/src/main/java/com/harrikirik/rescheck/App.kt
+++ ResCheck/src/main/java/com/harrikirik/rescheck/App.kt
@@ -1,10 +1,17 @@
 package com.harrikirik.rescheck
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.harrikirik.rescheck.util.isDebugBuild
 import timber.log.Timber
 
 class App : Application()  {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
 
     override fun onCreate() {
         super.onCreate()
